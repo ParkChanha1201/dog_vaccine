439eb70daee79518ba60302da407054b905bbcc0
박찬하 <chanhacraft@gmail.com>
2021-11-08 17:56:24 +0900

    first commit

---
+++ src/test/java/com/chcraft/dogvaccine/DogVaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chcraft.dogvaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogVaccineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
